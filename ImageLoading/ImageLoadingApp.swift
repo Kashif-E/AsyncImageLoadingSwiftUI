a49805f6337b812078ca78122609498ccece4b13
kashif mehmood <kashismails@gmail.com>
2022-03-19 23:09:25 +0500

    Initial Commit

---
+++ ImageLoading/ImageLoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLoadingApp.swift
+//  ImageLoading
+//
+//  Created by Kashif Mehmood on 19/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageLoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
